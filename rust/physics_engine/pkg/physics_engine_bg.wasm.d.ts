4d5c3f6623eb0f7585cfc2a9506494e85bb7d497
Lukas Campbell <77730288+Sparkylc2@users.noreply.github.com>
2025-05-25 20:27:30 +0100

    fixed

---
+++ rust/physics_engine/pkg/physics_engine_bg.wasm.d.ts
@@ -0,0 +1,21 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_constraintphysicsengine_free: (a: number, b: number) => void;
+export const constraintphysicsengine_new: () => number;
+export const constraintphysicsengine_add_body: (a: number, b: number, c: number, d: number, e: number, f: number) => number;
+export const constraintphysicsengine_add_distance_constraint: (a: number, b: number, c: number, d: number, e: number) => void;
+export const constraintphysicsengine_add_fixed_distance_constraint: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
+export const constraintphysicsengine_start_mouse_drag: (a: number, b: number, c: number) => number;
+export const constraintphysicsengine_update_mouse_position: (a: number, b: number, c: number) => void;
+export const constraintphysicsengine_end_mouse_drag: (a: number) => void;
+export const constraintphysicsengine_add_damping: (a: number) => void;
+export const constraintphysicsengine_update: (a: number, b: number) => void;
+export const constraintphysicsengine_body_position: (a: number, b: number) => any;
+export const constraintphysicsengine_body_angle: (a: number, b: number) => number;
+export const constraintphysicsengine_body_velocity: (a: number, b: number) => any;
+export const constraintphysicsengine_body_angular_velocity: (a: number, b: number) => number;
+export const constraintphysicsengine_set_gravity: (a: number, b: number, c: number) => void;
+export const constraintphysicsengine_set_iterations: (a: number, b: number) => void;
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_start: () => void;
